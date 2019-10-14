3c9b0850fef81bbb4fa157d8eb1e6b38f20317cb
Andrew Thorp <andrew.thorp.dev@gmail.com>
2019-10-14 17:52:17 -0400

    MVP works bitch

--- bbclient.py
+++ bbclient.py
@@ -1,6 +1,9 @@
-import sys
-import socket
 import array
+import sys
+import os
+import socket
+import signal
+import select
 import json
 import logging
 import queue
@@ -38,41 +41,73 @@ class Client(Thread):
         self.q_logger = q_logger
         self.message_add = message_add
         self.msg_queue = queue.Queue()
+        self.conn = None
         self.listening = True
         self.start()
 
     def run(self):
-        # Generate a key and then begin listining for messages
+        # Generate key
         try:
             if self.initiator:
                 self._initiate_keygen(self.recipient)
             else:
                 self._recv_keygen(self.recipient)
-            self.q_logger("Fetching connection")
 
+            # With the key generated, connect to peer
+            self.info("Fetching connection")
             cqc = bb84.get_CQCConnection(self.node_name)
             cqc.close()
 
             # Get connection to recipient
-            addr = cqc._appNet.hostDict[self.recipient].addr
-            self.conn = socket.socket(addr[0], addr[1], addr[2])
+            if self.initiator:
+                self.start_rx(cqc, 8080)
+                # self.start_tx(cqc, 8081)
+            else:
+                self.start_tx(cqc, 8080)
+                # self.start_rx(cqc, 8080)
 
-            self.message_add(("addr", addr[4]))
-            self.conn.connect(addr[4])
-
+            self.q_logger("Listening")
             # main loop
             while self.listening:
-                self.q_logger("Listening")
                 self._check_messages()
         except Exception as e:
             self.message_add(("err", e))
 
+    def start_rx(self, cqc, port):
+        """
+        self.rx = None
+        self.info("Opening socket")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.info("Biniding socket")
+        sock.bind(("", port))
+        self.info("Accepting connection")
+
+        sock.listen(1)
+        while self.rx is None:
+            self.info("Trying to accept connection: ({}, {})".format("", port))
+            (self.rx, addr) = sock.accept()
+        self.info("Connected to {}".format(addr))
+        """
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(("", 8080))
+        s.listen(1)
+        while self.conn is None:
+            (self.conn, addr) = s.accept()
+
+    def start_tx(self, cqc, port):
+        self.info("Opening socket")
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.info("Connecting")
+        self.info("Trying to connect to ({}, {})".format("", port))
+        self.conn.connect(("", 8080))
+
+    def info(self, msg):
+        self.message_add(("INFO", msg))
 
     def _initiate_keygen(self, recipient):
         init_msg = {"code": Q_KEYGEN, "sender": self.node_name}
         # with bb84.get_CQCConnection(self.node_name) as cqc:
         # cqc.sendClassical(recipient, json.dumps(init_msg).encode())
-
         try:
             k = bb84.initiate_keygen(
                 name=self.node_name, recipient=recipient, q_logger=self.q_logger
@@ -94,12 +129,12 @@ class Client(Thread):
             self._recv_keygen(initiator)
 
     def send_message(self, recipient, message):
-        self.msg_queue.put({"recipient": recipient, "body": message})
-        bb84.get_CQCConnection(self.node_name).closeClassicalServer()
+        # self.msg_queue.put({"recipient": recipient, "body": message})
+        self.msg_queue.put(message)
 
-    def _send_message(self, recipient, message):
-        key = self.key_map[recipient]
-        packet = json.dumps({"code": MESSAGE, "sender": self.node_name})
+    def _send_message(self, message):
+        key = self.key_map[self.recipient]
+        # packet = json.dumps({"code": MESSAGE, "sender": self.node_name})
 
         self.q_logger("Encrypting message")
         encrypted = bb84.encrypt(message, int(key))
@@ -110,26 +145,28 @@ class Client(Thread):
         self.conn.send(bytes(encrypted))
         self.q_logger("Message sent")
 
-    def _recv_message(self, sender, body):
+    def _recv_message(self, body):
+        sender = self.recipient
         key = self.key_map[sender]
         message = bb84.decrypt(body, int(key))
         self.message_add((sender, message.decode("utf-8")))
 
     def _check_messages(self):
-        message = None
         try:
             # Check for messages
-            self.q_logger("Listening for new stuff")
             rx_ready, tx_ready, _ = select.select([self.conn], [self.conn], [], 1)
+
             for rx in rx_ready:
-                self.q_logger("rx ready")
-                self.message_add(("rx: ", self.conn.recv(1024)))
+                raw = self.conn.recv(1024)
+                if len(raw) > 0:
+                    self._recv_message(raw)
+
             for tx in tx_ready:
-                self.q_logger("tx ready")
-                while self.msg_queue.not_empty():
+                while not self.msg_queue.empty():
                     msg = self.msg_queue.get()
-                    self._send
-                self.message_add(("rx: ", self.conn.recv(1024)))
+                    self._send_message(msg)
+                # self.message_add(("rx: ", self.conn.recv(1024)))
+            """
             # Check message type
             if message.get("code") == Q_KEYGEN:
                 self._recv_keygen(message["sender"])
@@ -138,12 +175,17 @@ class Client(Thread):
                 with bb84.get_CQCConnection(self.node_name) as rx:
                     body = rx.recvClassical()
                 self._recv_message(message["sender"], body)
+                """
 
         except Exception as e:
-            self.q_logger("Exception: {}".format(e))
-            _log.debug(e)
+            self.info("Exception: {}".format(e))
+            self.info("Listener stopped")
+            self.exit()
+
+            # _log.debug(e)
 
     def exit(self):
         # TODO figure out how the hell to close this...
-        self.listen= False
-        self.join()
+        if not self.conn is None:
+            self.conn.close()
+        os.kill(self.ident, signal.SIGTERM)
