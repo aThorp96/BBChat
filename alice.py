56894be85b188802ec10cb28223ea3795e5c6b43
Andrew Thorp <andrew.thorp.dev@gmail.com>
2019-10-29 07:44:21 -0400

    Use stantard basis

--- alice.py
+++ alice.py
@@ -8,49 +8,6 @@ length = 3 * key_size
 acceptable_error = 0.5
 recipient = "Bob"
 
-"""
-with CQCConnection("Alice") as Alice:
-    print("Connection made")
-
-    # Send bob key length
-    Alice.sendClassical(recipient, length)
-    confirmation = int.from_bytes(Alice.recvClassical(), byteorder="big")
-
-    # Get key, encode key, send to bob
-    key, qubits, bases = create_master_key(Alice, length)
-    key_bit_vect = BitVector(intVal=key)
-    # print("Key:           {}".format(bin(key)))
-    # print("Bases:         {}".format(bin(int(bases))))
-
-    for q in qubits:
-        Alice.sendQubit(q, recipient)
-
-    # receive bases used by Bob
-    bobs_bases = BitVector(bitlist=Alice.recvClassical())
-    # print("Bobs bases:    {}".format(bin(bobs_bases.int_val())))
-    correct_bases = ~bobs_bases ^ bases
-    correctness = correct_bases.count_bits() / length
-    # print("Correct:       {}".format(bin(int(correct_bases))))
-    print(correctness)
-
-    # Send bob correct bases
-    Alice.sendClassical(recipient, correct_bases[:])
-    key = truncate_key(key, length, correct_bases)
-
-    if validate_generated_key(length, acceptable_error, key) != OK:
-        raise Exception("Poor error rate: {}".format(1 - (len(key) / length)))
-        exit(0)
-
-    expected_verify, key = break_into_parts(key, key_size)
-    verification_bits = BitVector(bitlist=Alice.recvClassical())
-
-    if expected_verify != verification_bits:
-        Alice.sendClassical(recipient, TAMPERED)
-        pass
-    else:
-        Alice.sendClassical(recipient, OK)
-"""
-
 
 def initiate_keygen_standard(
     key_size=32, name="Alice", recipient="Bob", acceptable_error=0.5, q_logger=print
@@ -69,10 +26,11 @@ def initiate_keygen_standard(
 
         # Get key, encode key, send to bob
         key = random.randint(0, pow(2, length) - 1)
-        qubits, bases = encode_standard(conn, key, length)
+        qubits = encode_hadamard(conn, key)
+        bases = BitVector(intVal=1, size=length)
         key_bit_vect = BitVector(intVal=key)
-        q_logger("Key:           {}".format(bin(key)))
-        q_logger("Bases:         {}".format(bin(int(bases))))
+        q_logger("Key:      {}".format(bin(key)))
+        q_logger("Bases:    {}".format(bin(int(bases))))
 
         # Send all the qubits sequentially
         for q in qubits:
@@ -81,10 +39,10 @@ def initiate_keygen_standard(
 
         # receive bases used by Bob
         bobs_bases = BitVector(bitlist=conn.recvClassical())
-        q_logger("Bobs bases:    {}".format(bin(bobs_bases.int_val())))
+        q_logger("Bobs bases:{}".format(bin(bobs_bases.int_val())))
         correct_bases = ~bobs_bases ^ bases
         correctness = correct_bases.count_bits() / length
-        q_logger("Correct:       {}".format(bin(int(correct_bases))))
+        q_logger("Correct:   {}".format(bin(int(correct_bases))))
         q_logger(correctness)
 
         # Send bob correct bases
--- bob.py
+++ bob.py
@@ -3,53 +3,6 @@ from BitVector import BitVector
 
 enc_key = None
 
-"""
-with CQCConnection("Bob") as Bob:
-    print("Connection made")
-    # Receive lenth and initialize varaiables
-    length = int.from_bytes(Bob.recvClassical(), byteorder="big")
-    key_length = length / 3
-    Bob.sendClassical("Alice", length)
-    print(length)
-    qubits = [None] * length
-
-    for i in range(length):
-        qubits[i] = Bob.recvQubit()
-
-    key, bases = measure_random(qubits)
-    print("Key:     {}".format(bin(key)))
-    print("Bases:   {}".format(bin(bases.int_val())))
-
-    # Since we can only send indiviual numbers from 0-256,
-    # we have to split this up into a list of digits.
-    # Use slice to extract list from bitvector
-    Bob.sendClassical("Alice", bases[:])
-
-    correct_bases = BitVector(bitlist=Bob.recvClassical())
-    print("Correct: {}".format(bin(int(correct_bases))))
-
-    # Remove all incorrectly measured bits
-    key = truncate_key(key, length, correct_bases)
-
-    # Break into verification bits and final key
-    verification_bits, key = break_into_parts(key, key_length)
-    Bob.sendClassical("Alice", verification_bits[:])
-    enc_key = key
-
-    print("awaiting OK")
-    response = Bob.recvClassical()
-
-    if response == bytes(OK):
-        print("Key OK to use")
-    elif response == bytes(TAMPERED):
-        print("Key compromised!")
-        pass
-    print("Message recieved")
-
-    Bob.sendClassical("Alice", OK)
-
-"""
-
 
 def target_keygen_standard(name="Bob", initiator="Alice", q_logger=print):
     q_logger("Receiving keygen")
@@ -72,7 +25,8 @@ def target_keygen_standard(name="Bob", initiator="Alice", q_logger=print):
 
         q_logger("Recieved qubits!")
 
-        key, bases = measure_standard(qubits)
+        key = measure_hadamard(qubits)
+        bases = BitVector(intVal=1, size=length)
         q_logger("Key:     {}".format(bin(key)))
         q_logger("Bases:   {}".format(bin(bases.int_val())))
 
@@ -82,6 +36,7 @@ def target_keygen_standard(name="Bob", initiator="Alice", q_logger=print):
         conn.sendClassical(initiator, bases[:])
 
         correct_bases = BitVector(bitlist=conn.recvClassical())
+        correct_bases.pad_from_left(length - correct_bases.length())
         q_logger("Correct: {}".format(bin(int(correct_bases))))
 
         # Remove all incorrectly measured bits
